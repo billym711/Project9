5a52fb4b08bf0ceebbb477915c95da2d78a77501
billym711 <bmoorekid@gmail.com>
2023-11-16 14:59:18 -0500

    Pushing to github

---
+++ app/src/main/java/com/example/project9/PostsFragment.kt
@@ -0,0 +1,65 @@
+package com.example.project9
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
+import com.example.project9.databinding.FragmentPostsBinding
+
+
+class PostsFragment : Fragment() {
+
+    val TAG = "PostsFragment"
+    private var _binding: FragmentPostsBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentPostsBinding.inflate(inflater, container, false)
+        val view = binding.root
+        val viewModel : PostsViewModel by activityViewModels()
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        val adapter = PostsAdapter(this.requireContext())
+        binding.rvPosts.adapter = adapter
+        viewModel.posts.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.submitList(it)
+//                adapter.notifyDataSetChanged()
+            }
+        })
+
+        binding.fabCreate.setOnClickListener {
+          view.findNavController().navigate(R.id.action_postsFragment_to_createFragment)
+        }
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.toolbar.inflateMenu(R.menu.menu_posts)
+        binding.toolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.menu_profile -> {
+                    // Navigate to profile screen.
+                    view.findNavController().navigate(R.id.action_postsFragment_to_profileFragment)
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/project9/PostsViewModel.kt
@@ -0,0 +1,58 @@
+package com.example.project9
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.toObjects
+import com.example.project9.model.Post
+import com.example.project9.model.User
+
+class PostsViewModel : ViewModel() {
+
+
+    val TAG = "PostsViewModel"
+    var signedInUser: User? = null
+    private val _posts: MutableLiveData<MutableList<Post>> = MutableLiveData()
+    val posts: LiveData<List<Post>>
+        get() = _posts as LiveData<List<Post>>
+
+    init {
+
+        val  firestoreDB = FirebaseFirestore.getInstance()
+//        firestoreDB.collection("users")
+//            .document(FirebaseAuth.getInstance().currentUser?.uid as String)
+//            .get()
+//            .addOnSuccessListener { userSnapshot ->
+//                signedInUser = userSnapshot.toObject<User>()
+//                Log.i(TAG, "signed in user: $signedInUser")
+//            }
+//            .addOnFailureListener { exception ->
+//                Log.i(TAG, "Failure fetching signed in user", exception)
+//            }
+        var postsReference = firestoreDB
+            .collection("posts")
+            .limit(30)
+            .orderBy("creation_time_ms", Query.Direction.DESCENDING)
+
+        postsReference.addSnapshotListener { snapshot, exception ->
+            if (exception != null || snapshot == null) {
+                Log.e(TAG, "Exception when querying posts", exception)
+                return@addSnapshotListener
+            }
+            val postList = snapshot.toObjects<Post>()
+            _posts.value = postList as MutableList<Post>
+            for (post in postList) {
+                Log.i(TAG, "Post ${post}")
+            }
+        }
+    }
+
+    fun signOut() {
+        FirebaseAuth.getInstance().signOut()
+        signedInUser = null
+    }
+}
